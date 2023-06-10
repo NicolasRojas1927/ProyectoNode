4d2c49d40ec6cfe0e4de040d193ecad051a5ef3a
NicolasRojas1927 <nalexanderrojasucompensar.edu.co>
2023-06-10 18:05:57 -0500

    Proyecto_Node01

---
+++ index.js
@@ -0,0 +1,13 @@
+// Mensaje de Bienvenida
+
+console.log("Puerta de entrada al proyecto")
+
+//Parte intermedia del proyecto
+
+/*
+Apertura por medio de /*
+*/
+
+// Mensaje de despedida
+
+console.log("Adios")
